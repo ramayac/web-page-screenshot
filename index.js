17233f9eba69c0ce8feaf4656968693465a8bfd8
Rodrigo Amaya <506032+ramayac@users.noreply.github.com>
2021-01-31 19:06:18 -0600

    Endpoint for latest shot taken.

--- index.js
+++ index.js
@@ -2,16 +2,15 @@ const express = require('express');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const execFile = require('child_process').execFile;
-//const fs = require('fs');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 const FILENAME = 'screenshot.png';
-const SCREENSHOT_URL = 'https://duckduckgo.com/';
+const SCREENSHOT_URL = 'https://duckduckgo.com';
 
 /**
  * TODO: 
  * receive a param URL
- * api for latest shot taken.
  * improve readme.
  */
 
@@ -22,7 +21,17 @@ express()
   .get('/', async(req, res, next) => {
     res.status(200).send('ok').end();
   })
+  .get('/get_screenshot', async (req, res, next) => {
+    const screenshot = fs.readFileSync(FILENAME);
+    res.writeHead(200, {
+      'Content-Type': 'image/png',
+      'Content-Length': screenshot.length,
+    });
+    return res.end(screenshot);
+  })
   .get('/take_screenshot', async (req, res, next) => {
+    //Chromium flags: 
+    //https://peter.sh/experiments/chromium-command-line-switches/
     const browser = await puppeteer.launch({ 
       args: [
         '--no-sandbox',
