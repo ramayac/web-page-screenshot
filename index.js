7681227ea2291d387ad8b72673ae0db0d1532125
Rodrigo Amaya <506032+ramayac@users.noreply.github.com>
2021-01-31 18:52:48 -0600

    Fixed image conversion in Windows 10.

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3000;
 const FILENAME = 'screenshot.png';
 const SCREENSHOT_URL = 'https://duckduckgo.com/';
 
+/**
+ * TODO: 
+ * receive a param URL
+ * api for latest shot taken.
+ * improve readme.
+ */
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -15,11 +22,15 @@ express()
   .get('/', async(req, res, next) => {
     res.status(200).send('ok').end();
   })
-  .get('/screenshot', async (req, res, next) => {
-    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  .get('/take_screenshot', async (req, res, next) => {
+    const browser = await puppeteer.launch({ 
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ] 
+    });
     const page = await browser.newPage();
     //await page.setViewport({ width: 600, height: 800 });
-    //await page.goto(SCREENSHOT_URL || 'https://google.com');
     await page.goto(SCREENSHOT_URL);
 
     await page.screenshot({
@@ -37,8 +48,8 @@ express()
 
 function convert(filename) {
   return new Promise((resolve, reject) => {
-    const args = [filename, /*'-gravity', 'center', '-extent', '600x800',*/ '-colorspace', 'gray', '-depth', '8', filename];
-    execFile('convert', args, (error, stdout, stderr) => {
+    const args = ['convert', filename, '-gravity', 'center', /*'-extent', '600x800',*/ '-colorspace', 'gray', '-depth', '8', filename];
+    execFile('magick', args, (error, stdout, stderr) => {
       if (error) {
         console.error({ error, stdout, stderr });
         reject();
